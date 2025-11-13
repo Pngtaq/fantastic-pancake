2b1ec1075d3166a853b6d22f7506abb8a983941b
Pngtaq <110612377+Pngtaq@users.noreply.github.com>
2025-11-14 01:06:25 +0800

    buttons and dropdown menu

--- lib/views/pages/profile_page.dart
+++ lib/views/pages/profile_page.dart
@@ -9,22 +9,91 @@ class ProfilePage extends StatefulWidget {
 
 class _ProfilePageState extends State<ProfilePage> {
   TextEditingController controller = TextEditingController();
+  bool? isChecked = false;
+  bool isSwitched = false;
+  double sliderValue = 0.0;
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: Column(
-        children: [
-          TextField(
-            decoration: InputDecoration(border: OutlineInputBorder()),
-            controller: controller,
-            onEditingComplete: () {
-              setState(() {});
-            },
-          ),
-          Text(controller.text),
-        ],
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(border: OutlineInputBorder()),
+              controller: controller,
+              onChanged: (String text) {
+                setState(() {
+                  // The setState call tells Flutter to rebuild the UI
+                  // with the controller's latest text.
+                });
+              },
+            ),
+            Text(controller.text),
+            Checkbox(
+              tristate: true,
+              value: isChecked,
+              onChanged: (bool? value) {
+                setState(() {
+                  isChecked = value;
+                });
+              },
+            ),
+            CheckboxListTile(
+              tristate: true,
+              title: Text("Click me"),
+              value: isChecked,
+              onChanged: (bool? value) {
+                setState(() {
+                  isChecked = value;
+                  print(isChecked);
+                });
+              },
+            ),
+            Switch(
+              value: isSwitched,
+              onChanged: (bool value) {
+                setState(() {
+                  isSwitched = value;
+                  print(isSwitched);
+                });
+              },
+            ),
+            SwitchListTile.adaptive(
+              title: Text("hello"),
+              value: isSwitched,
+              onChanged: (bool value) {
+                setState(() {
+                  isSwitched = value;
+                  print(isSwitched);
+                });
+              },
+            ),
+            Slider(
+              value: sliderValue,
+              max: 10,
+              divisions: 10,
+              onChanged: (value) {
+                setState(() {
+                  sliderValue = value;
+                  print(sliderValue);
+                });
+              },
+            ),
+            InkWell(
+              splashColor: Colors.teal,
+              onTap: () {
+                print("hello");
+              },
+              child: Container(
+                height: 500,
+                width: double.infinity,
+                decoration: BoxDecoration(color: Colors.white12),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
