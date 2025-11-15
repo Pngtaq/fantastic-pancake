8d6a5e04a55a9a4035d69273843eb5aa93a23cd0
RaisonSalvador <raisonsalvador0@gmail.com>
2025-11-15 16:39:09 +0800

    feat: settings page

---
+++ lib/views/pages/settings_page.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+
+class SettingsPage extends StatefulWidget {
+  const SettingsPage({super.key});
+
+  @override
+  State<SettingsPage> createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  TextEditingController controller = TextEditingController();
+  bool? isChecked = false;
+  bool isSwitched = false;
+  double sliderValue = 0.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Settings'),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              TextField(
+                decoration: InputDecoration(border: OutlineInputBorder()),
+                controller: controller,
+                onChanged: (String text) {
+                  setState(() {
+                    // The setState call tells Flutter to rebuild the UI
+                    // with the controller's latest text.
+                  });
+                },
+              ),
+              Text(controller.text),
+              Checkbox(
+                tristate: true,
+                value: isChecked,
+                onChanged: (bool? value) {
+                  setState(() {
+                    isChecked = value;
+                  });
+                },
+              ),
+              CheckboxListTile(
+                tristate: true,
+                title: Text("Click me"),
+                value: isChecked,
+                onChanged: (bool? value) {
+                  setState(() {
+                    isChecked = value;
+                    print(isChecked);
+                  });
+                },
+              ),
+              Switch(
+                value: isSwitched,
+                onChanged: (bool value) {
+                  setState(() {
+                    isSwitched = value;
+                    print(isSwitched);
+                  });
+                },
+              ),
+              SwitchListTile.adaptive(
+                title: Text("hello"),
+                value: isSwitched,
+                onChanged: (bool value) {
+                  setState(() {
+                    isSwitched = value;
+                    print(isSwitched);
+                  });
+                },
+              ),
+              Slider(
+                value: sliderValue,
+                max: 10,
+                divisions: 10,
+                onChanged: (value) {
+                  setState(() {
+                    sliderValue = value;
+                    print(sliderValue);
+                  });
+                },
+              ),
+              InkWell(
+                splashColor: Colors.teal,
+                onTap: () {
+                  print("hello");
+                },
+                child: Container(
+                  height: 500,
+                  width: double.infinity,
+                  decoration: BoxDecoration(color: Colors.white12),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
