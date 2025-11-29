2c71f331473cd0974b2d23e4f8f6c312468da9b6
RaisonSalvador <raisonsalvador0@gmail.com>
2025-11-29 20:31:39 +0800

    remove: reset project

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,18 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
 
-void main() {
-  runApp(const MyApp());
+void main(){
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Flutter",
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(colorScheme: ColorScheme.dark()),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int currentIndex = 0;
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: currentIndex,
-        onTap: (index) {
-          setState(() {
-            currentIndex = index;
-          });
-        },
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.house), label: "Home"),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: "Settings",
-          ),
-        ],
-      ),
+      themeMode: ThemeMode.system,
+      theme: ThemeData(),
+      darkTheme: ThemeData(),
     );
   }
 }
