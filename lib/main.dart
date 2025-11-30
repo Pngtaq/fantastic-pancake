1c83c607d211b07d29bd2d2035424a3de1a0eba1
Pngtaq <110612377+Pngtaq@users.noreply.github.com>
2025-12-01 04:51:55 +0800

    feat: start scree

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,25 @@
+import "package:fantastic_pancake/start_screen.dart";
 import "package:flutter/material.dart";
 
 void main() {
-  runApp(MaterialApp(home: Scaffold()));
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 78, 13, 151),
+                Color.fromARGB(255, 107, 15, 168),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: StartScreen(),
+        ),
+      ),
+    ),
+  );
 }
