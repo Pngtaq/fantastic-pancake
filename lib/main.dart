6bf31cf64cd477d2c8dddc42d7943cd6ec315c8b
Pngtaq <110612377+Pngtaq@users.noreply.github.com>
2025-11-30 21:51:18 +0800

    removed: files and assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,5 @@
-import "package:fantastic_pancake/utils/theme/theme.dart";
 import "package:flutter/material.dart";
 
 void main() {
-  runApp(const App());
-}
-
-class App extends StatelessWidget {
-  const App({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      themeMode: ThemeMode.system,
-      theme: TAppTheme.lightTheme,
-      darkTheme: TAppTheme.darkTheme,
-    );
-  }
+  runApp(MaterialApp(home: Scaffold()));
 }
